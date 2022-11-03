22acdb47628b16900d50633477e6295134ac9e25
yiyuzhang0604 <yiyuzhang0604@gmail.com>
2022-11-03 09:26:16 -1000

    update main screen to add friend's name and add sticker

--- app/src/main/java/edu/northeastern/numad22fa_team8/MyFirebaseMessagingService.java
+++ app/src/main/java/edu/northeastern/numad22fa_team8/MyFirebaseMessagingService.java
@@ -5,10 +5,13 @@ import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.database.FirebaseDatabase;
 import android.provider.Settings;
 
+
+
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
     private String username;
     private String deviceId;
     private DatabaseReference dbRef;
+
 
     @Override
     public void onCreate() {
@@ -18,4 +21,6 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
                 getApplicationContext().getContentResolver(), Settings.Secure.ANDROID_ID);
 
     }
+
+
 }
--- app/src/main/java/edu/northeastern/numad22fa_team8/StickerApp.java
+++ app/src/main/java/edu/northeastern/numad22fa_team8/StickerApp.java
@@ -1,5 +1,7 @@
 package edu.northeastern.numad22fa_team8;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.os.Bundle;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -19,24 +21,30 @@ import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
-
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.messaging.FirebaseMessaging;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 public class StickerApp extends AppCompatActivity {
+
+    private static String SERVER_KEY;
     DatabaseReference dbRef;
-    Button btn_register;
-    EditText enterUserName;
+    Button btn_register, btn_send_sticker;
+    EditText enterUserName, enterFriendName;
     private FirebaseAuth mAuth;
+    private ImageView imageView1, imageView2, imageView3;
+    private static final String CHANNEL_ID = "CHANNEL_ID";
+    private static final String CHANNEL_NAME = "CHANNEL_NAME";
+    private static final String CHANNEL_DESCRIPTION = "CHANNEL_DESCRIPTION";
+    private Map<String, String> userNameToUserIdMap = new HashMap<>();
+    private Map<String, String> userIdToUserNameMap = new HashMap<>();
+
 
 
     @Override
@@ -45,8 +53,30 @@ public class StickerApp extends AppCompatActivity {
         setContentView(R.layout.activity_sticker_app);
         dbRef = FirebaseDatabase.getInstance().getReference();
         btn_register = findViewById(R.id.buttonUserRegister);
+        btn_send_sticker = findViewById(R.id.buttonSendSticker);
         enterUserName = findViewById(R.id.editTextEnterUserName);
+        enterFriendName = findViewById(R.id.editTextEnterFriendName);
         mAuth = FirebaseAuth.getInstance();
+        // get server key from google-service.json
+        SERVER_KEY = "key=AIzaSyCKl7WKMTFEpQHfjbAs6tZJr_X-EcH_Qik";
+        createNotificationChannel();
+        dbRef.child("users").get().addOnCompleteListener((task) -> {
+            HashMap<String, HashMap<String, String>> tempMap = (HashMap) task.getResult().getValue();
+            List<String> userNames = new ArrayList<>();
+
+            // populate user id and name
+            for (String userId : tempMap.keySet()) {
+                String userName = tempMap.get(userId).get("username");
+                if (userName == null || userName.equals(enterUserName.toString())) {
+                    continue;
+                }
+                userNames.add(userName);
+                userIdToUserNameMap.put(userId, userName);
+                userNameToUserIdMap.put(userName, userId);
+            }
+        });
+
+
 
         dbRef.setValue("here there").addOnSuccessListener(new OnSuccessListener<Void>() {
             @Override
@@ -57,6 +87,7 @@ public class StickerApp extends AppCompatActivity {
         }).addOnFailureListener(new OnFailureListener() {
             @Override
             public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getApplicationContext(), "Fail to connect to Database", Toast.LENGTH_SHORT).show();
 
             }
         }).addOnCompleteListener(new OnCompleteListener<Void>() {
@@ -94,7 +125,29 @@ public class StickerApp extends AppCompatActivity {
                             }
                         });
                 Toast.makeText(StickerApp.this, "You have successfully sign in!", Toast.LENGTH_SHORT).show();
+                btn_register.setText(inputUserName);
             }
         });
+
+        btn_send_sticker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!userNameToUserIdMap.containsKey(enterFriendName.toString())) {
+                    Toast.makeText(StickerApp.this, "Friend's name not exist!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(StickerApp.this, "Friend's name exist!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    public void createNotificationChannel() {
+        CharSequence name = CHANNEL_NAME;
+        String description = CHANNEL_DESCRIPTION;
+        int importance = NotificationManager.IMPORTANCE_DEFAULT;
+        NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+        channel.setDescription(description);
+        NotificationManager notificationManager = getSystemService(NotificationManager.class);
+        notificationManager.createNotificationChannel(channel);
     }
 }
