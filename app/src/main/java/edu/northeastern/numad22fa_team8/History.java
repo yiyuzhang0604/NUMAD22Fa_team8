16452692c7fb8c74689e86854acdcf0d9e614843
thisIsGloriaWu <gloriawu0924@gmail.com>
2022-11-05 13:18:01 -0700

    Modify sticker xml layout; Add History button

---
+++ app/src/main/java/edu/northeastern/numad22fa_team8/History.java
@@ -0,0 +1,14 @@
+package edu.northeastern.numad22fa_team8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class History extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+    }
+}
