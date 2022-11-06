5fd5eca46f6d326b95e7e7d8937a371e2ab0cebd
Yang(Raymond)He <65499114+dedehy@users.noreply.github.com>
2022-11-06 17:42:10 -0500

    updated notification

--- app/src/main/java/edu/northeastern/numad22fa_team8/StickerApp.java
+++ app/src/main/java/edu/northeastern/numad22fa_team8/StickerApp.java
@@ -2,6 +2,7 @@ package edu.northeastern.numad22fa_team8;
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
@@ -20,6 +21,8 @@ import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.core.content.ContextCompat;
 
 import android.provider.Settings;
@@ -63,7 +66,7 @@ public class StickerApp extends AppCompatActivity {
         SERVER_KEY = "key=AIzaSyCKl7WKMTFEpQHfjbAs6tZJr_X-EcH_Qik";
         userRegister();
         createNotificationChannel();
-//        sendNotification();
+        sendNotification();
         showStickersAvailable();
 
 
@@ -218,26 +221,32 @@ public class StickerApp extends AppCompatActivity {
         });
     }
 
-//    public void sendNotification() {
-//        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
-//                .setSmallIcon(R.mipmap.ic_launcher)
-//                .setContentTitle("Received New Message")
-//                .setPriority(NotificationCompat.PRIORITY_HIGH)
-//                .setAutoCancel(true)
-//                .setContentText(enterUserName.getText().toString() + "send you a message");
-//
-//        createNotificationChannel();
-//
-//        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
-//
-//        notificationManager.notify((int) System.currentTimeMillis(), builder.build());
-//
-//    }
+    public void sendNotification() {
+        Intent intent = new Intent(this, ReceiveHistoryActivity.class);
+
+        intent.putExtra("sender", enterSenderName.getText().toString());
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("Received New Message")
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent)
+                .setContentText(enterSenderName.getText().toString()+ "send you a message");
+
+        createNotificationChannel();
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+        notificationManager.notify((int) System.currentTimeMillis(), builder.build());
+
+    }
 
     public void createNotificationChannel() {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
         CharSequence name = CHANNEL_NAME;
         String description = CHANNEL_DESCRIPTION;
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
             int importance = NotificationManager.IMPORTANCE_DEFAULT;
             NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
             channel.setDescription(description);
